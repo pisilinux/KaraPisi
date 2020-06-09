0a91c62a2356375e74432e38706cdc912c11d6e2
blue-devil <bluedevil@sctzine.com>
2020-06-10 01:21:16 +0300

    crackle added

---
+++ security/wireless/bluetooth/crackle/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dobin("crackle")
+    pisitools.dodoc("COPYING", "README")
